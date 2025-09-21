b36803166f21460eb01b156f9a2e246b815fa710
Aziz <azizissa@students.alquds.edu>
2025-09-21 21:54:09 +0300

    assi_hello

---
+++ Python-Stack/_python/python_fundamentals/hello_world.py
@@ -0,0 +1,26 @@
+# 1-
+# print("hello my frinds !")
+# print("my name is :")
+
+
+# 2-
+name= ("aziz issa")
+# print(name)
+# print("my name is",name)
+# print("my name is"+name)
+
+
+
+# 3-
+num=1214
+# print("the number is",num)
+# print("the number is"+num)
+# print("the number is"+str(num))
+
+
+
+# 4-
+food1="sushi"
+food2="pizza"
+print("I love to eat {} and {}".format ( food1 , food2))
+print(f"I love to eat {food1} and {food2}")
