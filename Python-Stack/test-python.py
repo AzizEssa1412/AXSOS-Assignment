e4a4f39b9499e4e6f107251629f0d33e27882d3f
Aziz <azizissa@students.alquds.edu>
2025-09-21 15:13:19 +0300

    Python_stack

---
+++ Python-Stack/test-python.py
@@ -0,0 +1,33 @@
+# print("Hello World")
+
+
+# x=10
+# print(type(x))
+
+
+
+
+# +
+# ,
+
+# print("hello, my name is", str(x))
+# error
+# print("hello, my name is", x)
+
+
+# nume1=10
+# nume2="10"
+# print()
+
+
+# use to format 
+# print(f"my first name is {nume1},my second name is {nume2}")
+
+def click_btn():
+    print("hloooooo")
+
+click_btn()
+
+
+# if (cont > 20):
+    # print("the")
