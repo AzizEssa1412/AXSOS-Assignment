e81df0996f3ee33df9c519e6dd135740352b33ee
Aziz <azizissa@students.alquds.edu>
2025-09-22 15:47:23 +0300

    seco_assi_py

---
+++ Python-Stack/_python/python_fundamentals/Function_basic-1.py
@@ -0,0 +1,119 @@
+# 1- 
+def a():
+    return 5
+print(a())
+
+# 2- : 
+def a():
+    return 5
+print(a()+a())
+
+# 3-  
+def a():
+    return 5
+    return 10
+print(a())
+
+# 4- 
+def a():
+    return 5
+    print(10)
+print(a())
+
+# 5- 
+def a():
+    print(5)
+x = a()
+print(x)
+
+# 6- 
+def a(b,c):
+    print(b+c)
+print(a(1,2) + a(2,3))
+
+# 7- 
+def a(b,c):
+    return str(b)+str(c)
+print(a(2,5))
+
+# 8- 
+def a():
+    b = 100
+    print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(a())
+
+# 9-
+def a(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(a(2,3)) # print 7
+print(a(5,3)) # print 14
+print(a(2,3) + a(5,3)) # print 21
+
+# 10-
+def a(b,c):
+    return b+c
+    return 10
+print(a(3,5)) # the answer is 7
+
+
+# 11-
+b = 500
+print(b) # print 500
+def a():
+    b = 300
+    print(b)
+print(b) # print 500
+a() # print 300
+print(b) # print 500
+
+# 12- 
+b = 500
+print(b) # print 500
+def a():
+    b = 300
+    print(b)
+    return b
+print(b) # print 500
+a() # print 300
+print(b) # print 500
+
+# 13-
+b = 500
+print(b) # print 500
+def a():
+    b = 300
+    print(b)
+    return b
+print(b) # print 500
+b=a() # print 300 , and store the new value in b
+print(b) # print 300
+
+# 14-
+def a():
+    print(1)
+    b()
+    print(2)
+def b():
+    print(3)
+a() 
+
+# 15-
+def a():
+    print(1)
+    x = b()
+    print(x)
+    return 10
+def b():
+    print(3)
+    return 5
+y = a() 
+print(y) # print 10
