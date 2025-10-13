438e72a1dcf207500a7ab4fc3585c015f569312d
Aziz <azizissa@students.alquds.edu>
2025-10-13 11:44:04 +0300

    assi-djan-1shap

--- Python-Stack/django/django_fundamentals/Time_Display/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from time import gmtime  , strftime
-# Create your views here.
-def index(request):
-    context = {
-        "time": strftime(strftime("%B %D,%Y %I:%m %p"))
-    }
-    return render(request,'index.html',context)
