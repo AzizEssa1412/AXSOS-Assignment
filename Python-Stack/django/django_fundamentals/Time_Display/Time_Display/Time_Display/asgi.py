bd446efae1f6b01cf24d050cd55c42d7aeb620ce
Aziz <azizissa@students.alquds.edu>
2025-10-14 10:02:53 +0300

    Time_Display_assi

--- Python-Stack/django/django_fundamentals/Time_Display/Time_Display/Time_Display/asgi.py
+++ Python-Stack/django/django_fundamentals/Time_Display/Time_Display/Time_Display/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Time project.
+ASGI config for Time_Display project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Time.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Time_Display.settings')
 
 application = get_asgi_application()
---
+++ Python-Stack/django/django_fundamentals/Time_Display/Time_Display/myapp/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from time import  strftime
+# Create your views here.
+def index(request):
+    context = {
+        "time": strftime(strftime("%B %D,%Y %I:%m %p"))
+    }
+    return render(request,'index.html',context)
