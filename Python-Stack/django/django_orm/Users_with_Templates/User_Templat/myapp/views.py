96613a2f1963098246d68e7a64f44928ab411b29
Aziz <azizissa@students.alquds.edu>
2025-10-15 10:27:31 +0300

    user_with_template

--- Python-Stack/django/django_orm/Users_with_Templates/User_Templat/myapp/views.py
+++ Python-Stack/django/django_orm/Users_with_Templates/User_Templat/myapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.shortcuts import render,redirect
+from .models import User
+
+
+def index(request):
+    all_users = User.objects.all()
+    print(all_users)
+    return render(request, 'index.html', {'all_users': all_users})
+
+
+def add_user(request):
+    firstname = request.POST['first_name']
+    lastname = request.POST['last_name']
+    email = request.POST['email']
+    age = request.POST['age']
+    User.objects.create(first_name=firstname, last_name=lastname, email_address=email, age=age)
+    return redirect('/')
