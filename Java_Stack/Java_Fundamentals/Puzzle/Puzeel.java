20ad5a0fec64f8f1ea636d6014bc448a0054ff61
Aziz <azizissa@students.alquds.edu>
2025-11-05 00:13:34 +0200

    edit-puzeel-assi

--- Java_Stack/Java_Fundamentals/Puzzle/Puzeel.java
+++ Java_Stack/Java_Fundamentals/Puzzle/Puzeel.java
@@ -2,7 +2,7 @@ package Puzzle;
 
 import java.util.Arrays;
 import java.util.Random;
-public class Main {
+public class Puzeel {
     static Random random = new Random();
 
     public static void main(String [] args){
