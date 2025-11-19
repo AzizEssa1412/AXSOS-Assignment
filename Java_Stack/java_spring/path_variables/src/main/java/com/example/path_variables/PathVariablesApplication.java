c8037ba66d552c2807e865d334b30b3b13cbeb7d
Aziz <aziz.issa@students.alquds.edu>
2025-11-19 12:01:40 +0200

    hello-and-path-assi

---
+++ Java_Stack/java_spring/path_variables/src/main/java/com/example/path_variables/PathVariablesApplication.java
@@ -0,0 +1,13 @@
+package com.example.path_variables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathVariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathVariablesApplication.class, args);
+	}
+
+}
---
+++ Java_Stack/java_spring/path_variables/src/test/java/com/example/path_variables/PathVariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.path_variables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathVariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
