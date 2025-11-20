31798961b5d042b7d2b02635b775ecb0345271f5
Aziz <aziz.issa@students.alquds.edu>
2025-11-20 13:02:04 +0200

    edit-hello-assi

--- Java_Stack/java_spring/hello_human/src/main/java/com/example/hello_human/ServletInitializer.java
+++ Java_Stack/java_spring/hello_human/src/main/java/com/example/hello_human/ServletInitializer.java
@@ -10,4 +10,36 @@ public class ServletInitializer extends SpringBootServletInitializer {
 		return application.sources(HelloHumanApplication.class);
 	}
 
+	@RestController
+public class HumanController {
+
+
+    @RequestMapping("/")
+    public String index2(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "last", required = false) String last,  @RequestParam(value = "times", required = false) String times) {
+        if (name == null && last == null) {
+            return "Hello Human";
+        }
+        if (name != null && last != null) {
+            return "Hello   " + name+ " " + last;
+
+        }
+
+        return "Hello    " + name ;
+    }
+
+    @RequestMapping("/h")
+    public StringBuilder index3(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "times", required = false) String times) {
+        StringBuilder text= new StringBuilder("hello"+"  "+name+"  ");
+        if (times  != null){
+            for (int i = 0; i < Integer.parseInt(times); i++) {
+                text.append("hello").append("  ").append(name).append("  ");}
+
+            return text;
+        }
+        StringBuilder text2= new StringBuilder("name ");
+
+        return text2 ;
+    }
+
+	}
 }
