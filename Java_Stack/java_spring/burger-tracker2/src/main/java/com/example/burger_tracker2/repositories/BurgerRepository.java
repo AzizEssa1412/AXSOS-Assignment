9f457db1fc43620da8575f50c2d307aafc115b20
Aziz <aziz.issa@students.alquds.edu>
2025-11-23 07:52:47 +0200

    assi-Burger-Tracker-2

---
+++ Java_Stack/java_spring/burger-tracker2/src/main/java/com/example/burger_tracker2/repositories/BurgerRepository.java
@@ -0,0 +1,14 @@
+package main.java.com.example.burger_tracker2.repositories;
+
+import com.example.burgertracker2.models.Burger;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BurgerRepository extends CrudRepository<Burger, Long> {
+    List<Burger> findAll();
+    Optional<Burger> findById(Long id);
+}
