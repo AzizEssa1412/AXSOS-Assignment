5bdebdd45bfaa8fc1bbcc4d43a1ed7b4d0dde1e7
Aziz <aziz.issa@students.alquds.edu>
2025-12-03 10:29:33 +0200

    "MERN"edit-pred-assi

--- MERN_Stack/fundamentals_JavaScript/Predict_Destructuring.js
+++ MERN_Stack/fundamentals_JavaScript/Predict_Destructuring.js
@@ -48,3 +48,35 @@ console.log(hashedPassword);
 
 
 // 4. Original Code:
+const numbers = [8,2,3,5,6,1,67,12,2];
+const [, first] = numbers;         
+const [,,, second] = numbers;      
+const [,,,,,,,, third] = numbers;  
+
+console.log(first == second);
+console.log(first == third);
+
+// Output:
+// false
+// true
+
+
+//-------------------------------------------------------------------------------
+
+
+// 5. Original Code:
+const lastTest = { key: 'value', secondKey: [1,5,1,8,3,3] }
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [, willThisWork] = secondKey;
+
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+// Output:
+// value
+// [1,5,1,8,3,3]
+// 1
+// 5
