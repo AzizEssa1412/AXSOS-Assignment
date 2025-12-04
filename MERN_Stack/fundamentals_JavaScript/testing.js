c94bf3da112fa249b79c705895ff858a5bbdb628
Aziz <aziz.issa@students.alquds.edu>
2025-12-04 09:16:21 +0200

    Pract-Algo-JS

--- MERN_Stack/fundamentals_JavaScript/testing.js
+++ MERN_Stack/fundamentals_JavaScript/testing.js
@@ -35,23 +35,45 @@
 // let
 // const
 
-function twoSum(arr, target)
-{
-    let left = 0, right = arr.length - 1;
-    while (left < right) {
-        let sum = arr[left] + arr[right];
+// function twoSum(arr, target)
+// {
+//     let left = 0, right = arr.length - 1;
+//     while (left < right) {
+//         let sum = arr[left] + arr[right];
 
-        if (sum === target)
-            return left + " " + right;
-        else if (sum < target)
-            left++; 
-        else
-            right--; 
-    }
-    return false;
+//         if (sum === target)
+//             return left + " " + right;
+//         else if (sum < target)
+//             left++; 
+//         else
+//             right--; 
+//     }
+//     return false;
+// }
+
+// let arr = [ 2, 7, 11, 13, 20 ];
+// let target = 18;
+
+// console.log(twoSum(arr, target));
+
+
+//===================================================================================
+
+
+// let x = 1;
+// let y = 2;
+// let z == 3;
+
+// console.log (x+x);
+// console.log (y-z);
+// console.log (z*x);
+
+
+//===================================================================================
+
+
+for (let i = 0; i < 3; i++) {
+    const name = "Khaled";
 }
 
-let arr = [ 2, 7, 11, 13, 20 ];
-let target = 18;
-
-console.log(twoSum(arr, target));
+const name = names[index];
