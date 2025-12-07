5d332100ee2589bed6ef33c8072921c473e540f3
Aziz <aziz.issa@students.alquds.edu>
2025-12-07 17:01:17 +0200

    "MERN"fin-edit-assi-PropItUp

--- MERN_Stack/React/prop_it_up/src/App.jsx
+++ MERN_Stack/React/prop_it_up/src/App.jsx
@@ -6,11 +6,10 @@ function App() {
   return (
     <>
     <div>
-        <SaleItem name={"Doe , Jane"} age={45} color={"black"} />
+        <SaleItem name={"Aziz , Issa"} age={22} color={"green"} />
         <SaleItem name={"Smith , John"} age={88} color={"brown"} />
-        <SaleItem name={"Fillmore , Millard"} age={50} color={"green"} />
+        <SaleItem name={"Fillmore , Millard"} age={50} color={"black"} />
         <SaleItem name={"Smith , Maria"} age={62} color={"yellow"} />
-
     </div>
     </>
   )
--- MERN_Stack/React/prop_it_up/src/components/SaleItem.jsx
+++ MERN_Stack/React/prop_it_up/src/components/SaleItem.jsx
@@ -5,11 +5,12 @@ function SaleItem(props) {
 const [age, setAge] = useState(0);
 return (
     <>
-    fghfghfgh
+    
     <div>
         <h1> {props.name}</h1>
         <h2> {age}</h2>
         <h3> {props.color} </h3>
+        <h2>Test--Test--Test--Test--Test--Test</h2>
         <button onClick={()=>setAge(age+1)}>Click Me</button>
     </div>
     </>
