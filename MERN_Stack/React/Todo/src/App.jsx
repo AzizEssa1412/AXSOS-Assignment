3f9bba20c203819bb76af95a2f53a6a4ea0fc2a2
Aziz <aziz.issa@students.alquds.edu>
2025-12-22 01:02:37 +0200

    "MERN"Todo-List-assig

---
+++ MERN_Stack/React/Todo/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import TodoForm from "./Components/TodoForm.jsx";
+import List from './components/list.jsx';
+
+
+function App() {
+  const [list, setList] = useState([])
+
+  return (
+    <>
+        <TodoForm  setList={setList} />
+        <List list={list} setList={setList} />
+    </>
+  )
+}
+
+export default App
