660aa5001a6ac414764d61411e58b625eec808ca
Aziz <aziz.issa@students.alquds.edu>
2025-12-22 05:03:38 +0200

    "MERN"Pokemon_API-assig

---
+++ MERN_Stack/React/Pokemon_API/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import List from "./Components/List.jsx";
+// eslint-disable-next-line no-unused-vars
+import {useState} from 'react'
+
+
+function App() {
+
+  return (
+    <>
+        <List/>
+    </>
+  )
+}
+
+export default App
